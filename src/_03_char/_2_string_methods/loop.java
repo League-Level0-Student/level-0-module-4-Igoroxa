c16731c5535344600cf212bb177a6bee2f71cf78
root <root@08e1e2b37386>
2020-07-02 23:44:22 +0000

    module 5

---
+++ src/_03_char/_2_string_methods/loop.java
@@ -0,0 +1,11 @@
+package _03_char._2_string_methods;
+
+public class loop {
+
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+	}
+}
--- src/_99_extra/SimpleCalculator.java
+++ src/_99_extra/SimpleCalculator.java
@@ -9,13 +9,26 @@ public class SimpleCalculator {
 		// 1. Get 2 numbers from the user and convert them to integer.
 String num1 = JOptionPane.showInputDialog("ENTER IN A NUMBER");
 String num2 = JOptionPane.showInputDialog("Enter in a Number");
+int intnum1 = Integer.parseInt(num1); 
+int intnum2 = Integer.parseInt(num2);
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
 		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "Sub (not subscribe) subtract", "Multiply", "Divide" },
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "subtract", "Multiply", "Divide" },
 				null);
 
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
+if (operation == 0) {
+	add(intnum1,intnum2);
+}
+if (operation == 0) {
+	subtract(intnum1,intnum2);
+}
+if (operation == 0) {
+	multiply(intnum1,intnum2);
+}
+if (operation == 0) {
+	divide(intnum1,intnum2);
+}
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
 
@@ -24,10 +37,20 @@ String num2 = JOptionPane.showInputDialog("Enter in a Number");
 	//    HINT: use 'static void add(int num1, num2) { ... }
 	static void add(int num1, int num2) {
 		JOptionPane.showMessageDialog(null, num1 + " + " + num2 + " = " + (num1 + num2));
-		static void add(int num1, int num2) {
-			JOptionPane.showMessageDialog(null, num1 + " + " + num2 + " = " + (num1 - num2));
+		}
+		static void subtract(int num1, int num2) {
+			JOptionPane.showMessageDialog(null, num1 + " - " + num2 + " = " + (num1 - num2));
+			}
+		static void multiply(int num1, int num2) {
+			JOptionPane.showMessageDialog(null, num1 + " * " + num2 + " = " + (num1 * num2));
+			}
+		static void divide(int num1, int num2) {
+			JOptionPane.showMessageDialog(null, num1 + " / " + num2 + " = " + (num1 / num2));
+			}
+		
+	
 				
 			
-	}
+	
 	// 4. Create similar methods for subtraction, multiplication and division.
 }
